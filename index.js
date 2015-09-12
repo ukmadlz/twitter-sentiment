10ec2e64f158cdf815886d47ab2f779d15163434
Mike Elsmore <mike@elsmore.me>
2015-09-12 23:25:02 +0200

    Change two a 2 route system to allow for pre-caching of DATA

--- index.js
+++ index.js
@@ -41,8 +41,73 @@ server.route({
       include_rts: false,
     };
 
-    client.get('statuses/user_timeline', params, function(error, tweets, response){
-        if (!error) {
+    var tweetArray = [];
+
+    Cloudant({account:vcapServices.cloudantNoSQLDB[0].credentials.username, password:vcapServices.cloudantNoSQLDB[0].credentials.password}, function(er, cloudant) {
+      var database = cloudant.db.use(twitterHandle);
+
+      // https://f65d7aca-996b-43b6-b273-8d7feb6dbb07-bluemix.cloudant.com/ukmadlz/_design/lookups/_view/timestamps?limit=20&reduce=false&inclusive_end=true&start_key=1440583652000&end_key=1440683652000
+      var viewParams = {
+          inclusive_end: true,
+          include_docs:true
+        };
+
+      if(req.query.start_date)
+        viewParams.start_key = req.query.start_date*1000;
+      if(req.query.end_date)
+        viewParams.end_key = req.query.end_date*1000;
+
+      database.view('lookups', 'timestamps', viewParams, function(err, body) {
+        if (!err) {
+
+          var tweetText = '';
+          body.rows.forEach(function(doc) {
+            tweetText = tweetText + ' ' + doc.doc.text;
+          });
+
+          var frequency = body.rows.length;
+
+          request.post({
+            headers: {'content-type' : 'application/x-www-form-urlencoded'},
+            url:     'http://battlehack.jakelprice.com/api/nlp',
+            body:    "payload=" + tweetText
+          }, function(error, response, body){
+            if(response.statusCode == 400){
+              reply({score:0,frequency:0});
+            } else {
+              body = JSON.parse(body);
+              body.frequency = frequency;
+              body.score = body.value;
+              reply(body);
+            }
+          });
+
+        } else {
+          console.log(err);
+        }
+      });
+    });
+
+  },
+});
+
+server.route({
+  method: 'GET',
+  path: '/new',
+  handler: function(req, reply) {
+
+    var client = new Twitter({
+      consumer_key: TWITTER.consumer_key,
+      consumer_secret: TWITTER.consumer_secret,
+      // This can be generated via OAuth or from the http://apps.twitter.com dashboard
+      access_token_key: req.query.authKey,
+      access_token_secret: req.query.authToken
+    });
+
+    var twitterHandle = req.query.account.toLowerCase();
+
+    var tweetArray = [];
+
           Cloudant({account:vcapServices.cloudantNoSQLDB[0].credentials.username, password:vcapServices.cloudantNoSQLDB[0].credentials.password}, function(er, cloudant) {
             cloudant.db.create(twitterHandle, function(err, body) {
 
@@ -57,73 +122,47 @@ server.route({
                 database.insert({"views": { "timestamps": { "map": "function (doc) {\n  var d = new Date(doc.created_at);\n  emit(d.getTime(), 1);\n}" } } }, '_design/lookups', function(err, body, header) {});
               });
 
-              for(var i=0;i<tweets.length;i++){
-                database.insert(tweets[i], tweets[i].id_str, function(err, body, header) {
-                  if (err)
-                    return console.log('['+twitterHandle+'.insert] ', err.message)
-                  //
-                  // console.log('you have inserted the tweet.')
-                  // console.log(body)
-                });
-              }
+              function getTwitter(id,counter) {
 
-              // https://f65d7aca-996b-43b6-b273-8d7feb6dbb07-bluemix.cloudant.com/ukmadlz/_design/lookups/_view/timestamps?limit=20&reduce=false&inclusive_end=true&start_key=1440583652000&end_key=1440683652000
-              var viewParams = {
-                  inclusive_end: true,
-                  include_docs:true
+                var counterLength = 10;
+
+                if(counter<counterLength) {
+                  counter++;
+                }
+
+                var params = {
+                  screen_name: twitterHandle,
+                  count: 200,
+                  include_rts: false,
                 };
 
-              if(req.query.start_date)
-                viewParams.start_key = req.query.start_date*1000;
-              if(req.query.end_date)
-                viewParams.end_key = req.query.end_date*1000;
-
-              database.view('lookups', 'timestamps', viewParams, function(err, body) {
-                if (!err) {
-                  var tweetText = ''
-                  body.rows.forEach(function(doc) {
-                    tweetText = tweetText + ' ' + doc.doc.text;
-                  });
-
-                  var frequency = body.rows.length;
-
-                  var opts = {
-                    url: 'http://battlehack.jakelprice.com/api/nlp',
-                    form:{
-                      payload: tweetText,
-                    },
-                  };
-
-                  request.post({
-                    headers: {'content-type' : 'application/x-www-form-urlencoded'},
-                    url:     'http://battlehack.jakelprice.com/api/nlp',
-                    body:    "payload=" + tweetText
-                  }, function(error, response, body){
-                    if(response.statusCode == 400){
-                      reply({});
-                    } else {
-                      body = JSON.parse(body);
-                      body.frequency = frequency;
-                      body.score = body.value;
-                      reply(body);
-                    }
-                  });
-
-                } else {
-                  console.log(err);
+                if(id>0) {
+                  params.max_id = id;
                 }
-              });
+
+                client.get('statuses/user_timeline', params, function(error, tweets, response){
+
+                  console.log('--Batch '+id+'--');
+
+                  for(var j=0;j<tweets.length;j++){
+                    database.insert(tweets[j], tweets[j].id_str, function(err, body, header) {
+                      if (err)
+                        return console.log('['+twitterHandle+'.insert][' + j + '] ', err.message);
+                    });
+                    if((j+1)==tweets.length && counter < counterLength) {
+                      getTwitter(tweets[j].id_str,counter);
+                    }
+                  }
+                });
+
+              }
+
+              getTwitter(0,0);
+
+              reply({thanks:"bro"});
 
             });
-          });
-
-        } else {
-          console.log(error);
-          console.log(response);
-          reply('index', { title: 'ERROR' });
-        }
       });
-
   },
 });
 
