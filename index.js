bd8fb25e00da2946e26f621941bc27f6815dd24f
Mike Elsmore <mike@elsmore.me>
2015-09-12 16:45:16 +0200

    And the basic Twitter module is done

--- index.js
+++ index.js
@@ -1,10 +1,110 @@
+'use strict';
+
+if (!process.env.VCAP_SERVICES) {
+  require('dotenv').load();
+}
+
+var vcapServices = JSON.parse(process.env.VCAP_SERVICES);
+
 var Hapi = require('hapi');
+var Twitter = require('twitter');
+var Cloudant = require('cloudant');
+var request = require('request');
 
 var server = new Hapi.Server({ debug: { request: ['error'] } });
-process.env.VCAP_APP_HOST || 'localhost', process.env.VCAP_APP_PORT || 3000,
+
 server.connection({
   host: process.env.VCAP_APP_HOST || 'localhost',
   port: process.env.VCAP_APP_PORT || 3000,
+});
+
+var TWITTER = JSON.parse(process.env.TWITTER);
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: function(req, reply) {
+
+    var client = new Twitter({
+      consumer_key: TWITTER.consumer_key,
+      consumer_secret: TWITTER.consumer_secret,
+      // This can be generated via OAuth or from the http://apps.twitter.com dashboard
+      access_token_key: req.query.authKey,
+      access_token_secret: req.query.authToken
+    });
+
+    var twitterHandle = req.query.account;
+
+    var params = {
+      screen_name: twitterHandle,
+      count: 200,
+    };
+
+    client.get('statuses/user_timeline', params, function(error, tweets, response){
+        if (!error) {
+          Cloudant({account:vcapServices.cloudantNoSQLDB[0].credentials.username, password:vcapServices.cloudantNoSQLDB[0].credentials.password}, function(er, cloudant) {
+            cloudant.db.create(twitterHandle, function() {
+              var database = cloudant.db.use(twitterHandle);
+
+              database.get('_design/lookups', function(err, body) {
+                if (!err)
+                  console.log(body);
+                database.insert({"views": { "timestamps": { "map": "function (doc) {\n  var d = new Date(doc.created_at);\n  emit(d.getTime(), 1);\n}" } } }, '_design/lookups', function(err, body, header) {});
+              });
+
+              for(var i=0;i<tweets.length;i++){
+                database.insert(tweets[i], tweets[i].id_str, function(err, body, header) {
+                  if (err)
+                    return console.log('['+twitterHandle+'.insert] ', err.message)
+                  //
+                  // console.log('you have inserted the tweet.')
+                  // console.log(body)
+                });
+              }
+
+              // https://f65d7aca-996b-43b6-b273-8d7feb6dbb07-bluemix.cloudant.com/ukmadlz/_design/lookups/_view/timestamps?limit=20&reduce=false&inclusive_end=true&start_key=1440583652000&end_key=1440683652000
+              database.view('lookups', 'timestamps', {start_key: req.query.start_key, end_key: req.query.end_key, inclusive_end: true, include_docs:true}, function(err, body) {
+                if (!err) {
+                  var tweetText = ''
+                  body.rows.forEach(function(doc) {
+                    tweetText = tweetText + ' ' + doc.doc.text;
+                  });
+
+                  var frequency = body.rows.length;
+
+                  var opts = {
+                    url: 'http://battlehack.jakelprice.com/api/nlp',
+                    form:{
+                      payload: tweetText,
+                    },
+                  };
+
+                  request.post({
+                    headers: {'content-type' : 'application/x-www-form-urlencoded'},
+                    url:     'http://battlehack.jakelprice.com/api/nlp',
+                    body:    "payload=" + tweetText
+                  }, function(error, response, body){
+                    body = JSON.parse(body);
+                    body.frequency = frequency;
+                    reply(body);
+                  });
+
+                } else {
+                  console.log(err);
+                }
+              });
+
+            });
+          });
+
+        } else {
+          console.log(error);
+          console.log(response.req._header);
+          reply('index', { title: 'ERROR' });
+        }
+      });
+
+  },
 });
 
 server.start(function() {
